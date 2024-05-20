1142eb602baa27c158e920d8dac031d00f5c7d6c
ignassssss <ignacy.szadkowski@gmail.com>
2024-05-20 12:13:05 +0200

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,8 +15,6 @@ public class Main {
                 {apple_tree.symbol, '0', apple_tree.symbol}
         };
 
-        int test;
-
         int how_many_trees = 0;
         for (int i = 0; i <3; i++) {
             for (int j = 0; j <3; j++) {
